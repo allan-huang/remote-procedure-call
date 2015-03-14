8eb68933d91a1ed4f2c99f82e9e35e15f4497e44
allan-huang <rawrencehuang@gmail.com>
2015-03-14 22:53:59 +0800

    Fix the startup script files in the bin folder

--- bin/startup.bat
+++ bin/startup.bat
@@ -3,23 +3,23 @@ ECHO OFF
 SET LIB_PATH=
 SET LIB_PATH=%LIB_PATH%;.\rpc-client.properties
 SET LIB_PATH=%LIB_PATH%;.\rpc-server.properties
-SET LIB_PATH=%LIB_PATH%;.\rpc-simulator.properties
+rem SET LIB_PATH=%LIB_PATH%;.\rpc-simulator.properties
 
 SET LIB_PATH=%LIB_PATH%;.\lib\commons-lang3-3.3.2.jar
 SET LIB_PATH=%LIB_PATH%;.\lib\gson-2.3.1.jar
 SET LIB_PATH=%LIB_PATH%;.\lib\jzlib-1.1.3.jar
-SET LIB_PATH=%LIB_PATH%;.\lib\log4j-api-2.0.2.jar
-SET LIB_PATH=%LIB_PATH%;.\lib\log4j-core-2.0.2.jar
-SET LIB_PATH=%LIB_PATH%;.\lib\log4j-jcl-2.0.2.jar
-SET LIB_PATH=%LIB_PATH%;.\lib\log4j-slf4j-impl-2.0.2.jar
-SET LIB_PATH=%LIB_PATH%;.\lib\netty-all-4.0.25.Final.jar
-SET LIB_PATH=%LIB_PATH%;.\lib\slf4j-api-1.7.7.jar
-SET LIB_PATH=%LIB_PATH%;.\lib\ychuang-rpc.jar
-SET LIB_PATH=%LIB_PATH%;.\lib\test-ychuang-rpc.jar
+SET LIB_PATH=%LIB_PATH%;.\lib\log4j-api-2.2.jar
+SET LIB_PATH=%LIB_PATH%;.\lib\log4j-core-2.2.jar
+SET LIB_PATH=%LIB_PATH%;.\lib\log4j-jcl-2.2.jar
+SET LIB_PATH=%LIB_PATH%;.\lib\log4j-slf4j-impl-2.2.jar
+SET LIB_PATH=%LIB_PATH%;.\lib\netty-all-4.0.26.Final.jar
+SET LIB_PATH=%LIB_PATH%;.\lib\slf4j-api-1.7.10.jar
+SET LIB_PATH=%LIB_PATH%;.\lib\ychuang-rpc-1.0.1.jar
+rem SET LIB_PATH=%LIB_PATH%;.\lib\test-ychuang-rpc-1.0.1.jar
 
 SET JAVA_OPTS=-Xmx1024m -Xms1024m -XX:PermSize=64m -XX:MaxPermSize=64m
 
 ECHO ON
 
-rem %JAVA_HOME%/bin/java %JAVA_OPTS% -cp %LIB_PATH% tw.me.ychuang.rpc.Main
-%JAVA_HOME%/bin/java %JAVA_OPTS% -cp %LIB_PATH% test.tw.me.ychuang.rpc.Simulator
+%JAVA_HOME%/bin/java %JAVA_OPTS% -cp %LIB_PATH% tw.me.ychuang.rpc.Main
+rem %JAVA_HOME%/bin/java %JAVA_OPTS% -cp %LIB_PATH% test.tw.me.ychuang.rpc.Simulator
--- bin/startup.sh
+++ bin/startup.sh
@@ -9,25 +9,28 @@ LIB_PATH=`dirname $0`
 CLASSES=$LIB_PATH
 CLASSES=$CLASSES:$LIB_PATH/rpc-client.properties
 CLASSES=$CLASSES:$LIB_PATH/rpc-server.properties
-CLASSES=$CLASSES:$LIB_PATH/rpc-simulator.properties
+#CLASSES=$CLASSES:$LIB_PATH/rpc-simulator.properties
 
 CLASSES=$CLASSES:$LIB_PATH/lib/commons-lang3-3.3.2.jar
 CLASSES=$CLASSES:$LIB_PATH/lib/gson-2.3.1.jar
 CLASSES=$CLASSES:$LIB_PATH/lib/jzlib-1.1.3.jar
-CLASSES=$CLASSES:$LIB_PATH/lib/log4j-api-2.0.2.jar
-CLASSES=$CLASSES:$LIB_PATH/lib/log4j-core-2.0.2.jar
-CLASSES=$CLASSES:$LIB_PATH/lib/log4j-jcl-2.0.2.jar
-CLASSES=$CLASSES:$LIB_PATH/lib/log4j-slf4j-impl-2.0.2.jar
-CLASSES=$CLASSES:$LIB_PATH/lib/netty-all-4.0.25.Final.jar
-CLASSES=$CLASSES:$LIB_PATH/lib/slf4j-api-1.7.7.jar
-CLASSES=$CLASSES:$LIB_PATH/lib/ychuang-rpc.jar
+CLASSES=$CLASSES:$LIB_PATH/lib/log4j-api-2.2.jar
+CLASSES=$CLASSES:$LIB_PATH/lib/log4j-core-2.2.jar
+CLASSES=$CLASSES:$LIB_PATH/lib/log4j-jcl-2.2.jar
+CLASSES=$CLASSES:$LIB_PATH/lib/log4j-slf4j-impl-2.2.jar
+CLASSES=$CLASSES:$LIB_PATH/lib/netty-all-4.0.26.Final.jar
+CLASSES=$CLASSES:$LIB_PATH/lib/slf4j-api-1.7.10.jar
+CLASSES=$CLASSES:$LIB_PATH/lib/ychuang-rpc-1.0.1.jar
+#CLASSES=$CLASSES:$LIB_PATH/lib/test-ychuang-rpc-1.0.1.jar
 
 if [ "$MODE" == "" ] || [ "$MODE" == "b" ] ; then
 	echo "Starting up YCHuang RPC in background."	
 	nohup $JAVA_HOME/bin/java -Xms1024m -Xms1024m -XX:PermSize=64m -XX:MaxPermSize=64m -cp $CLASSES tw.me.ychuang.rpc.Main > $RPC_HOME/logs/rpc.out 2>&1 &	
+	#nohup $JAVA_HOME/bin/java -Xms1024m -Xms1024m -XX:PermSize=64m -XX:MaxPermSize=64m -cp $CLASSES test.tw.me.ychuang.rpc.Simulator > $RPC_HOME/logs/rpc.out 2>&1 &	
 else [ "$MODE" == "f" ]
 	echo "Starting up YCHuang RPC in foreground."	
 	$JAVA_HOME/bin/java -Xms1024m -Xms1024m -XX:PermSize=64m -XX:MaxPermSize=64m -cp $CLASSES tw.me.ychuang.rpc.Main
+	#$JAVA_HOME/bin/java -Xms1024m -Xms1024m -XX:PermSize=64m -XX:MaxPermSize=64m -cp $CLASSES test.tw.me.ychuang.rpc.Simulator
 fi
 
 exit 0
