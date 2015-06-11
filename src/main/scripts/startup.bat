bba1694664f0635f34e5eacaf1050af959936c67
allan-huang <rawrencehuang@gmail.com>
2015-06-11 16:31:57 +0800

    Add a log4j2.xml classpath to the windows batch script and linux bash script.

--- src/main/scripts/startup.bat
+++ src/main/scripts/startup.bat
@@ -1,12 +1,15 @@
-ECHO OFF
+echo OFF
 
-SET LIB_PATH=
-SET LIB_PATH=%LIB_PATH%;.\rpc-client.properties
-SET LIB_PATH=%LIB_PATH%;.\rpc-server.properties
-SET LIB_PATH=%LIB_PATH%;.\lib\*
+set LIB_PATH=
+set LIB_PATH=%LIB_PATH%;.\log4j2.xml
+set LIB_PATH=%LIB_PATH%;.\rpc-client.properties
+set LIB_PATH=%LIB_PATH%;.\rpc-server.properties
+set LIB_PATH=%LIB_PATH%;.\lib\*
 
-SET JAVA_OPTS=-Xmx1024m -Xms1024m -XX:PermSize=64m -XX:MaxPermSize=64m
+set MAIN_CLASS=tw.me.ychuang.rpc.Main
 
-ECHO ON
+set JAVA_OPTS=-Xmx1024m -Xms1024m -XX:PermSize=64m -XX:MaxPermSize=64m
 
-%JAVA_HOME%\bin\java %JAVA_OPTS% -cp %LIB_PATH% tw.me.ychuang.rpc.Main
+echo ON
+
+%JAVA_HOME%\bin\java %JAVA_OPTS% -cp %LIB_PATH% %MAIN_CLASS%
--- src/main/scripts/startup.sh
+++ src/main/scripts/startup.sh
@@ -3,20 +3,22 @@
 MODE=$1
 JAVA_HOME=/usr/java/latest
 RPC_HOME=/usr/local/rpc
+MAIN_CLASS='tw.me.ychuang.rpc.Main'
 
 LIB_PATH=`dirname $0`
 
 CLASSES=$LIB_PATH
+CLASSES=$CLASSES:$LIB_PATH/log4j2.xml
 CLASSES=$CLASSES:$LIB_PATH/rpc-client.properties
 CLASSES=$CLASSES:$LIB_PATH/rpc-server.properties
 CLASSES=$CLASSES:$LIB_PATH/lib/*
 
 if [ "$MODE" == "" ] || [ "$MODE" == "b" ] ; then
 	echo "Starting up YCHuang RPC in background."	
-	nohup $JAVA_HOME/bin/java -Xmx1024m -Xms1024m -XX:PermSize=64m -XX:MaxPermSize=64m -cp $CLASSES tw.me.ychuang.rpc.Main > $RPC_HOME/logs/rpc.out 2>&1 &	
+	nohup $JAVA_HOME/bin/java -Xmx1024m -Xms1024m -XX:PermSize=64m -XX:MaxPermSize=64m -cp $CLASSES $MAIN_CLASS > $RPC_HOME/logs/rpc.out 2>&1 &	
 else [ "$MODE" == "f" ]
 	echo "Starting up YCHuang RPC in foreground."	
-	$JAVA_HOME/bin/java -Xmx1024m -Xms1024m -XX:PermSize=64m -XX:MaxPermSize=64m -cp $CLASSES tw.me.ychuang.rpc.Main
+	$JAVA_HOME/bin/java -Xmx1024m -Xms1024m -XX:PermSize=64m -XX:MaxPermSize=64m -cp $CLASSES $MAIN_CLASS
 fi
 
 exit 0
